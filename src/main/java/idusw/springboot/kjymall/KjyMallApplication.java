e9a95affefde9869f513ee6b13d204475b811987
Kimhasa <wlsdud11457@gmail.com>
2024-03-19 10:34:33 +0900

    Revert "Revert "first commit""
    This reverts commit 58076cb86ec01db5f362e9fcf30c61fa1217f8c7.

---
+++ src/main/java/idusw/springboot/kjymall/KjyMallApplication.java
@@ -0,0 +1,13 @@
+package idusw.springboot.kjymall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // Spring - Annotation : java, lombok, spring
+public class KjyMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KjyMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/idusw/springboot/kjymall/KjyMallApplicationTests.java
@@ -0,0 +1,13 @@
+package idusw.springboot.kjymall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KjyMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
