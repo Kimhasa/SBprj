367ef1970d75deadc51b765cc8f2d201f179966b
Kimhasa <wlsdud11457@gmail.com>
2024-04-02 12:50:17 +0900

    Add - 5주자

---
+++ src/main/java/idusw/springboot/kjymall/controller/MyController.java
@@ -0,0 +1,16 @@
+package idusw.springboot.kjymall.controller;
+
+import com.fasterxml.jackson.databind.deser.impl.PropertyValueBuffer;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+    @GetMapping(value={"my", "my/"})
+    public String getMy(HttpServletRequest request){
+        // /my?fav=값 or /my/?fav=값
+        return "<h1>" + request.getParameter("fav") + "</h1>"; // view 지정 없이 바로 응답
+    }
+
+}
